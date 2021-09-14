f13a52cf1117651e1c7ad507a7220deceb374f1c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-14 13:27:19 -0700

    fixed reaction button prop passing error

--- src/components/ReactionButtons/index.tsx
+++ src/components/ReactionButtons/index.tsx
@@ -1,32 +1,8 @@
-import { useState, useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '@app/store/hooks';
-import { selectReactionsById, setReaction, upsertReaction } from '@app/store/reactionsSlice';
+import { useAppDispatch } from '@app/store/hooks';
+import { upsertReaction } from '@app/store/reactionsSlice';
 import { Heart, Bookmark } from 'react-feather';
-import styled from '@emotion/styled';
 
 import AnimatedIcon from '../AnimatedIcon';
-
-const StyledHeart = styled(Heart)<{
-  heart: boolean
-}>`
-  /* width: ${props => props.heart ? '30px' : '24px'};
-  height: ${props => props.heart ? '30px' : '24px'};
-  object-fit: fill; */
-  color: rgba(127, 29, 29, 0.7);
-  fill: rgb(153, 27, 27);
-  fill-opacity: ${props => props.heart ? '1' : '0'};
-  transition: fill-opacity 150ms linear;
-
-`;
-
-const StyledBookmark = styled(Bookmark)<{
-  bookmark: boolean
-}>`
-  color: rgba(0, 0, 0, 0.7);
-  fill: rgb(17, 24, 39);
-  fill-opacity: ${props => props.bookmark ? '1' : '0'};
-  transition: fill-opacity 150ms linear;
-`;
 
 interface ReactionButtonProps {
   url: string;
@@ -38,44 +14,54 @@ interface ReactionButtonProps {
 export default function ReactionButtons({
   url,
   date,
-  bookmarked,
-  hearted
+  bookmarked = false,
+  hearted = false
 }: ReactionButtonProps) {
-  const [bookmark, rawSetBookmark] = useState(bookmarked);
-  const [heart, rawSetHeart] = useState(hearted);
   const dispatch = useAppDispatch();
-
   
   function setHeart() {
     dispatch(upsertReaction({
       url: url,
       date: date,
-      heart: !heart,
-      bookmark: bookmark
+      heart: !hearted,
+      bookmark: bookmarked
     }));
-    rawSetHeart(!heart);
   };
 
   function setBookmark() {
     dispatch(upsertReaction({
       url: url,
       date: date,
-      heart: heart,
-      bookmark: !bookmark
+      heart: hearted,
+      bookmark: !bookmarked
     }));
-    rawSetBookmark(!bookmark);
   }
-
-
-
 
   return (
     <div className="flex gap-x-2 md:gap-x-4">
       <button onClick={setHeart}>
-        <AnimatedIcon activated={heart} className=""><StyledHeart heart={heart} /></AnimatedIcon>
+        <AnimatedIcon activated={hearted}>
+            <Heart
+              style={{
+                color: 'rgba(127, 29, 29, 0.7)',
+                fill: 'rgb(153, 27, 27)',
+                fillOpacity: hearted ? 1 : 0,
+                transition: 'fill-opacity 150ms linear'
+              }}
+            />
+        </AnimatedIcon>
       </button>
       <button onClick={setBookmark}>
-        <AnimatedIcon activated={bookmark} className=""><StyledBookmark bookmark={bookmark} className="" /></AnimatedIcon>
+        <AnimatedIcon activated={bookmarked}>
+            <Bookmark
+              style={{
+                color: 'rgba(0, 0, 0, 0.7)',
+                fill: 'rgb(17, 24, 39)',
+                fillOpacity: bookmarked ? 1 : 0,
+                transition: 'fill-opacity 150ms linear'
+              }}
+            />
+        </AnimatedIcon>
       </button>  
     </div>
   );

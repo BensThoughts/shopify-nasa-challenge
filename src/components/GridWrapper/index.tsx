fc9ed141cfbc1667d703a4ba72711cac5768cad8
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-13 17:01:24 -0700

    responsive Grid component

--- src/components/GridWrapper/index.tsx
+++ src/components/GridWrapper/index.tsx
@@ -9,14 +9,19 @@ const Wrapper = styled.div<{
   row-gap: 3rem;
   grid-template-columns: ${({ charWidth = 90 }) => `1fr min(${charWidth}ch, 100%) 1fr`};
   * {
-    grid-column: 2;
+    grid-column: 1 / -1;
+  }
+  @media (min-width: 768px) {
+    * {
+      grid-column: 2;
+    }
   }
 `;
 
-interface GridWrapperProps {
-  charWidth?: number
+type GridWrapperProps = {
+  charWidth?: number,
   className?: string,
-  children: ReactNode;
+  children: ReactNode,
 }
 
 export default function GridWrapper({

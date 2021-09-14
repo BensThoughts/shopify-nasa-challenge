28c0ef2221717b8ff99fb454001628804f6dbcea
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-13 23:13:27 -0700

    full screen images

--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
 import { HTMLAttributes } from 'react';
-import { useTimeoutFn } from 'react-use';
+
 
 import Image from 'next/image';
 
 import Date from '@app/components/Date';
-import useProgressiveImg from '@app/utils/hooks/useProgressiveImg';
-import AnimatedPulse from '@app/components/AnimatedPulse';
+import ImageModal from '@app/components/ImageModal';
 
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -28,59 +27,18 @@ const toBase64 = (str: string) =>
     ? Buffer.from(str).toString('base64')
     : window.btoa(str)
 
-
-
-const Card = styled.div`
-  width: 500px;
-  height: 100%;
-  display: grid;
-  grid-template-rows: repeat(12, 1fr);
-  grid-template-columns: 1;
-  position: relative;
-  row-gap: 10px;
-  align-items: center;
-  justify-items: center;
-`;
-
-const ImageWrap = styled.div`
-  width: 400px;
-  height: 400px;
-  grid-row: 1 / 8;
-
-`;
-
 const ImageBlur = styled(Image)<{
   blur: boolean
 }>`
-  height: 400px;
+  /* height: 400px; */
+  /* width: 400px; */
+  z-index: 3;
+  height: 100%;
   width: 400px;
-  z-index: 2;
+  object-fit: scale-down;
   /* grid-area: 1 / 1 / span 3 / 1; */
   filter: ${(props) => props.blur ? 'blur(20px)' : 'none'};
   transition: ${(props) => props.blur ? 'none' : 'filter 0.2s ease-out'};
-`;
-
-const InfoBox = styled.div`
-  width: 500px;
-  height: 100%;
-  position: relative;
-  grid-row: 6 / -1;
-  z-index: 1;
-  border-radius: 15px;
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  padding-top: 50px;
-  /* grid-area: 8 / -1 / -1 / 1; */
-  background: rgb(99, 110, 114);
-`;
-
-const TitleBox = styled.div`
-
-`;
-
-const DescriptionBox = styled.div`
-
 `;
 
 type ImageCardProps = {
@@ -101,65 +59,54 @@ export default function ImageCard({
   description,
   ...rest
 }: ImageCardProps) {
-  // const [loaded] = useProgressiveImg(url);
   const [blur, setBlur] = useState(true);
-  // const [, , resetBlur] = useTimeoutFn(() => setBlur(false), 500);
-
-  // if (loaded) {
-  //   resetBlur();
-  // }
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Card {...rest}>
-        <ImageWrap>
-          <ImageBlur
+    <>
+      <ImageModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        title={title}
+        description={description}
+      >
+        <Image
+          src={url}
+          alt={title}
+          width="960"
+          height="960"
+          placeholder="blur"
+          onClick={() => setIsOpen(false)}
+          
+          layout="intrinsic"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+        />
+      </ImageModal>
+      <div className="p-2 flex flex-col border-solid border-2 border-gray-500 border-opacity-60 shadow-md max-w-xl bg-gray-300">
+        <div className="bg-black">
+          <button aria-label="Open image in full screen" onClick={() => setIsOpen(true)}>
+            <ImageBlur
             src={url}
             alt={title}
-            width="400"
-            height="400"
+            width="960"
+            height="960"
             placeholder="blur"
             onLoad={() => setBlur(false)}
             blur={blur}
-            layout="responsive"
+            layout="intrinsic"
             blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
-          />
-        </ImageWrap>
-        
-        <InfoBox>
-          <div className="font-bold">{title}</div>
-          <div><Date dateString={date} /></div>
-          <DescriptionBox>{description}</DescriptionBox>
-        </InfoBox>
-    </Card>
+            />
+          </button>
+        </div>
+  
+        <div className="font-bold">
+          {title}
+        </div>
+        <div className="flex">
+          <Date dateString={date} />
+        </div>
+        <div>{copyright}</div>
+      </div>
+    </>
   );
 }
-
-
-
-
-{/* <Image src={url} alt={title} width="700" height="475" placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`} /> */}
-
-
-// {blur
-//   ? <AnimatedPulse />
-//   : <picture>
-//       <source srcSet={hdurl} media="(min-width: 740px)" />
-//         <img src={url} alt={title} width="400" height="400"/>
-//     </picture>
-// }
-
-
-// {loaded
-//   ? <AnimatedPulse />
-//   : <img
-//      src={url}
-//      alt={title}
-//      width="400"
-//      height="400"
-//      style={{
-//        filter: blur ? 'blur(20px)' : 'none',
-//        transition: blur ? 'none' : 'filter 0.2s ease-out'
-//      }}
-//      className="object-contain"
-//    />
-// }
---
+++ src/components/ImageModal/index.tsx
@@ -0,0 +1,85 @@
+import { Fragment, ReactNode } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import styled from '@emotion/styled';
+
+const ImageWrap = styled.div`
+  width: screen;
+  max-width: 800px;
+`;
+
+interface ImageModalProps {
+  title: string;
+  description?: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: ReactNode;
+}
+
+export default function ImageModal({
+  title,
+  description,
+  isOpen = false,
+  onClose,
+  children
+}: ImageModalProps) {
+  return (
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        onClose={onClose}
+        className="fixed inset-0 z-10 overflow-y-auto"
+      >
+        <div className="text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-30"
+            entered="opacity-30"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-30"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black"/>
+          </Transition.Child>
+          <span
+            className="inline-block h-screen align-middle"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div
+            onClick={onClose}
+            className="inline-block w-full overflow-hidden text-left align-middle transition-all transform bg-black shadow-xl">
+
+
+              <div className="p-3 flex flex-col items-center w-screen">
+                <div className="">
+                  <h3
+                    className="text-lg mb-3 font-medium leading-6 text-gray-200"
+                  >
+                    {title}
+                  </h3>
+                  <ImageWrap tabIndex={0}>
+                    {children}
+                  </ImageWrap>
+
+                </div>
+
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+    </Dialog>
+    </Transition>
+  );
+}

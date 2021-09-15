73897f883a1fbfa30fe7d83ebe29e09d84c39b56
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-14 19:08:20 -0700

    added seo

---
+++ src/next-seo.config.ts
@@ -0,0 +1,25 @@
+const NextSeoConfig =  {
+  title: 'Shopify Nasa Challenge',
+  description: 'An Instagram clone showing beautiful photos from the NASA APOD API',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IE',
+    url: 'https://shopify-nasa-challenge.netlify.app',
+    site_name: 'Shopify Nasa Challenge',
+    title: 'Shopify Nasa Challenge',
+    description: 'Beautiful photos from NASA, infinitely scrolled',
+    images: [{
+      url: 'https://res.cloudinary.com/bensthoughts/image/upload/v1631670713/shopify-nasa-challenge/og-image_v6enq5.jpg',
+      width: 1200,
+      height: 627,
+      alt: 'Astronaut in space with earth background'
+    }]
+  },
+  twitter: {
+    handle: '@BensThoughts',
+    site: '@BensThoughts',
+    cardType: 'summary_large_image'
+  }
+}
+
+export default NextSeoConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux';
-import { store } from '@app/store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import { DefaultSeo } from 'next-seo';
+import { store } from '@app/store/store';
+let persister = persistStore(store);
+
+import NextSeoConfig from '@app/next-seo.config';
 import Navbar from '@app/components/Layout/Navbar';
 
-let persister = persistStore(store);
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persister}>
-        <Navbar className="h-14" />
-        <div className="pt-14">
-          <Component {...pageProps} />
-        </div>
-      </PersistGate>
-    </Provider>
+    <>
+      <DefaultSeo {...NextSeoConfig} />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persister}>
+          <Navbar className="h-14" />
+          <div className="pt-14">
+            <Component {...pageProps} />
+          </div>
+        </PersistGate>
+      </Provider>
+    </>
   );
 }
 export default MyApp

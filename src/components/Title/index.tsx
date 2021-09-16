10db2ac6720323164d2b8a620f319d2f9b410564
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-16 12:21:35 -0700

    fixed Title font size

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,19 +1,20 @@
 import styled from '@emotion/styled';
 
 const Title = styled.h1`
+  max-width: 100%;
   font-family: 'Orbitron', sans-serif;
   color: rgb(var(--color-text-primary));
   font-style: oblique;
   text-shadow: 2px 2px 2px #5c5f72;;
-  letter-spacing: 10px;
-  font-size: 2rem;
+  /* letter-spacing: 10px; */
+  font-size: 2.2rem;
   @media (min-width: 640px) {
     font-size: 3rem;
-    line-height: 3.5rem;
+    /* line-height: 3.5rem; */
   }
   @media (min-width: 768px) {
     font-size: 4rem;
-    line-height: 4.5rem;
+    /* line-height: 4.5rem; */
   }
 `;
 

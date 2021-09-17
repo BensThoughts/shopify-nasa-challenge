c4c82d9750438db541bf1da394a7654cc32d004e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-17 11:02:35 -0700

    contact page started

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,10 +1,22 @@
 import MaxWidthWrapper from '@app/components/MaxWidthWrapper';
 import ContactCard from '@app/components/ContactCard';
+import GridWrapper from '@app/components/GridWrapper';
+import Title from '@app/components/Title';
 
 export default function ContactPage() {
   return (
     <MaxWidthWrapper>
-      <ContactCard className="w-full h-full justify-center items-center" />
+      <GridWrapper>
+        <section aria-label="page title" className="w-full flex flex-col items-center justify-center my-5">
+          <Title>Contact Me</Title>
+          <div>
+            <h2 className="italic md:font-light text-base sm:text-lg md:text-xl">Contact me on these platforms</h2>
+          </div>
+        </section>
+        <section>
+          <ContactCard className="w-full h-full justify-center items-center" />
+        </section>
+      </GridWrapper>
     </MaxWidthWrapper>
   );
 }
